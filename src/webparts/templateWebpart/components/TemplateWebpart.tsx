db4eae7866f410b2bdbea57de65bc8fa916a870d
Karl-Einar Bergstrøm <karl-einarb@outlook.com>
2022-04-27 15:53:22 +0200

    Restructured the code

--- src/webparts/templateWebpart/components/TemplateWebpart.tsx
+++ src/webparts/templateWebpart/components/TemplateWebpart.tsx
@@ -1,14 +1,19 @@
 import * as React from 'react';
 import styles from './TemplateWebpart.module.scss';
 import { ITemplateWebpartProps } from './ITemplateWebpartProps';
-import { isEqual, sortBy } from 'lodash'
+import { isEqual, sortBy, times } from 'lodash'
 import Sjablong from 'sjablong'
 import handlebars from 'handlebars';
-import mockData from './Data.js'
+// import mockData from './Data.js'
 import template from './Template.js'
+import { nanoid } from 'nanoid';
+import axios, { AxiosRequestConfig } from 'axios';
 
 export interface ITemplateWebpartState {
-  errors: any[];
+  id: string,
+  isLoading: boolean,
+  propErrors: string[]
+  error: any,
   data: any;
   html: string;
 }
@@ -27,8 +32,11 @@ export default class TemplateWebpart extends React.Component<ITemplateWebpartPro
     State
   */
   state = {
-    errors: [],
-    data: {},
+    id: nanoid(),
+    isLoading: false,
+    propErrors: [],
+    error: undefined,
+    data: undefined,
     html: ''
   }
 
@@ -36,7 +44,7 @@ export default class TemplateWebpart extends React.Component<ITemplateWebpartPro
     LifeCycle hooks
   */
   // Runs only when the component first loads
-  componentDidMount = () => {
+  componentDidMount = async () => {
     this.debug('=== Webpart Mounted ===');
     this.debug('Props', this.props)
 
@@ -44,15 +52,16 @@ export default class TemplateWebpart extends React.Component<ITemplateWebpartPro
     element.innerHTML = template;
     console.log('Element', element)
 
-    this.validateProps(undefined, undefined)
+    await this.runActions(undefined, undefined);
   }
 
   // Runs data for the components updates and triggers a re-render
-  componentDidUpdate = (prevProps, prevState) => {
+  componentDidUpdate = async (prevProps : ITemplateWebpartProps, prevState : ITemplateWebpartState) => {
     this.debug('=== Webpart Updated ===');
     this.debug('Props', this.props)
     this.debug('State', this.state)
-    this.validateProps(prevProps, prevState)
+
+    await this.runActions(prevProps, prevState);
   }
 
   /*
@@ -63,145 +72,236 @@ export default class TemplateWebpart extends React.Component<ITemplateWebpartPro
     console.log(...args)
   }
 
-  private validateProps (prevProps, prevState) {
-    /*
-      Setup
-    */
-    // The handlebars import must be set as a variable here to make sure webpack don't renames it
-    // The reason for this is that injection scripts with handlebar-helpers would not know what the name is
-    const Handlebars = handlebars;  
+  isAllEqual(obj1 : Object, obj2 : Object, properties: string[]) : Boolean {
+    if(!obj1 || !obj2 || !properties) return false;
 
-    /*
-      Validate that the props are ok
-    */
-    const _errors = [];
-    if(!this.props.type) _errors.push('type must be provided');
-    if(!this.props.method) _errors.push('method must be provided');
-    if(this.props.type === 'basic') {
-      if(!this.props.username) _errors.push('username must be provided when authentication basic is used');
-      if(!this.props.password) _errors.push('password must be provided when authentication basic is used');
-    } else if (this.props.type === 'oauth') {
-      if(!this.props.oauthClientId) _errors.push('oauthClientId must be provided when authentication oauth is used');
-      if(!this.props.oauthAuthorityUrl) _errors.push('oauthAuthorityUrl must be provided when authentication oauth is used');
-      if(!this.props.oauthScopes) _errors.push('oauthScopes must be provided when authentication oauth is used');
+    let isAllEqual = true;
+
+    for(const prop of properties) {
+      if(isEqual(obj1[prop], obj2[prop])) continue;
+      isAllEqual = false;
+      break
     }
-    if(!this.props.dataUrl) _errors.push('dataUrl must be provided');
-    // if(this.props.dataUrl && !urlRegex.test(this.props.dataUrl)) _errors.push('dataUrl is not in a valid url format');
-    if(!this.props.templateUrl && !this.props.templateString) _errors.push('templateUrl or templateString must be provided');
-    
-    if(this.props.templateUrl && !urlRegex.test(this.props.templateUrl)) _errors.push('templateUrl is not in a valid url format');
-    if(this.props.errorTemplateUrl && !urlRegex.test(this.props.errorTemplateUrl)) _errors.push('errorTemplateUrl is not in a valid url format');
-    if(this.props.loadingTemplateUrl && !urlRegex.test(this.props.loadingTemplateUrl)) _errors.push('loadingTemplateUrl is not in a valid url format');
 
-    let previousErrors : String[] = prevState?.errors || [];
-    const sortedErrors = sortBy(_errors, (i) => i)
+    return isAllEqual;
+  }
+
+  private async validateProps (props) {
+    const errors = [];
+
+    if(!this.props.type) errors.push('type must be provided');
+    if(!this.props.method) errors.push('method must be provided');
+    if(this.props.type === 'basic') {
+      if(!this.props.username) errors.push('username must be provided when authentication basic is used');
+      if(!this.props.password) errors.push('password must be provided when authentication basic is used');
+    } else if (this.props.type === 'oauth') {
+      if(!this.props.oauthClientId) errors.push('oauthClientId must be provided when authentication oauth is used');
+      if(!this.props.oauthAuthorityUrl) errors.push('oauthAuthorityUrl must be provided when authentication oauth is used');
+      if(!this.props.oauthScopes) errors.push('oauthScopes must be provided when authentication oauth is used');
+    }
+    if(!this.props.dataUrl) errors.push('dataUrl must be provided');
+    // if(this.props.dataUrl && !urlRegex.test(this.props.dataUrl)) errors.push('dataUrl is not in a valid url format');
+    if(!this.props.templateUrl && !this.props.templateString) errors.push('templateUrl or templateString must be provided');
+    
+    if(this.props.templateUrl && !urlRegex.test(this.props.templateUrl)) errors.push('templateUrl is not in a valid url format');
+    if(this.props.errorTemplateUrl && !urlRegex.test(this.props.errorTemplateUrl)) errors.push('errorTemplateUrl is not in a valid url format');
+    if(this.props.loadingTemplateUrl && !urlRegex.test(this.props.loadingTemplateUrl)) errors.push('loadingTemplateUrl is not in a valid url format');
+
+    return errors;
+  }
+
+  private async runActions(prevProps : ITemplateWebpartProps, prevState : ITemplateWebpartState) {
+    /*
+      If there are errors, just return
+    */
+    if(this.state.error) return;
+    if(this.state.isLoading) return;
+
+    /*
+      Validate the properties
+      This must be done everytime before doing anything else
+    */
+    // Retreive all current prop errors
+    const propErrors = await this.validateProps(this.props);
+    // Retreive all previous prop errors
+    let previousErrors : String[] = prevState?.propErrors || [];
+    // Sort the error sets
+    const sortedErrors = sortBy(propErrors, (i) => i)
     previousErrors = sortBy(previousErrors, (i) => i)
 
-    // Protects against inifite loop
+    // If the errors are not the same, update the store
     if(!isEqual(sortedErrors, previousErrors)) {
-      return this.setState({errors: _errors});
+      this.setState({
+        propErrors
+      })
+      return;
     }
-    if(_errors.length > 0) return;
+    // If there are prop errors there is no need to continue
+    if(propErrors.length > 0) return;
+
 
     /*
-      Determine what template to use
+      Determine what actions must be done
     */
-    // TODO: Add support for templateUrl
-    const fullTemplate = this.props.templateString;
-    let bodyTemplateString = '';
+    // Check if the last render had errors
+    const hadPropErrors = prevState?.propErrors?.length > 0;
+    const hadErrors = prevState?.error
 
-    if(_errors.length > 0) return this.setState({errors: _errors});
-    /*
-      Load the template into a HTML element
-    */
-    const element = document.createElement('div')
-    try {
-      element.innerHTML = fullTemplate;
-    } catch (err) {
-      _errors.push('Could not parse template into HTML element: ' + err.message)
-    }
-
-    if(_errors.length > 0) return this.setState({errors: _errors});
-    /*
-      Load the template
-    */
-    try {
-      this.debug('Retreiving x-template')
-      let elements = element.querySelectorAll("[type='x-template']");
-      if(!elements || elements.length === 0) throw new Error('Could not find any x-template elements in the template');
-      if(!elements[0].innerHTML) throw new Error('x-template cannot be empty');
-      this.debug(`Found ${elements.length} elements`, elements)
-      bodyTemplateString = elements[0].innerHTML;
-    } catch (err) {
-      _errors.push(`Error parsing template:'\n${err.message}`)
+    // Check if it is time to authenticate again
+    let mustAuthenticate = false;
+    if(this.props.type !== 'anynomous') {
+      mustAuthenticate = !this.isAllEqual(this.props, prevProps, ['type', 'username', 'password']) || hadErrors;
     }
 
+    // Check if data must be retreived
+    const mustFetchData = !this.isAllEqual(this.props, prevProps, ['dataUrl']) || this.state.data === undefined;
+
+    // Check if rerender is required
+    const mustRerender = !this.isAllEqual(this.props, prevProps, ['templateUrl', 'templateString']) || mustAuthenticate || hadErrors;
+
+
+    if(isEqual(prevProps, this.props) && isEqual(prevState, this.state)) {
+      this.debug('Both previous and current state and props are identical, returning early');
+      return;
+    }
+
+    if(!mustAuthenticate && !mustFetchData && !mustRerender) {
+      this.debug('No actions needed, returning early');
+      return;
+    }
+
+    this.debug('Required actions:');
+    this.debug('Must autheticate: ', mustAuthenticate);
+    this.debug('Must mustFetchData: ', mustFetchData);
+    this.debug('Must mustRerender: ', mustRerender);
+
     /*
-      Parse and load all x-head scripts
+      Run all actions
     */
     try {
-      this.debug('Retreiving x-head')
-      const elements = element.querySelectorAll("[type='x-head']");
-      this.debug(`Found ${elements.length} elements`, elements)
-      for (let i = 0; i < elements.length; i++) {
-        // Find all script elements 
-        const scripts = elements[i].getElementsByTagName('script');
-  
-        for (let y = 0; y < scripts.length; y++) {
-          // Create a new script element, it will not run if just appending the parsed scripts
-          const script = document.createElement("script");
-          script.type = 'text/javascript'
-          script.async = true;
-          script.innerHTML = scripts[y].innerHTML;
-          script.id = `x-head-${Math.random().toString()}`
-          // Append it to the head
-          document.head.appendChild(script);
-        }
+      // Retreive the template body
+      const templateBody = this.getTemplateBody(this.props);
+
+      // TODO: Add authentication
+
+      // Retreive data
+      const data = mustFetchData ? await this.fetchData(this.props) : this.state.data;
+
+      let html = this.state.html;
+      if(mustRerender) {
+        // Parse the template as a HTML element
+        const templateElement = this.parseTemplateToHTMLElement(templateBody);
+
+        // Retreive the x-template text from element
+        const xTemplate = this.retreiveXTemplateFromHTML(templateElement);
+
+        // Register x-head script elements
+        this.registerXHeadScripts(templateElement);
+
+        // Run any x-inject code
+        this.runXInjectCode(templateElement);
+
+        // Run the rerender
+        html = mustRerender ? this.renderTemplate(xTemplate, data) : this.state.html;
       }
-    } catch (err) {
-      _errors.push(`Error parsing parsing x-head:'\n${err.message}`)
-    }
-    if(_errors.length > 0) return this.setState({errors: _errors});
 
+      // Update the state
+      this.setState({
+        data,
+        html,
+        isLoading: false
+      })
+    } catch (err) {
+      console.error('An error has occured', err)
+      this.setState({
+        error: err.message
+      })
+    }
+  }
+
+  private getTemplateBody(props : ITemplateWebpartProps) {
+    // TODO: Add support for URL
+    return props.templateString;
+  }
+
+  private parseTemplateToHTMLElement(template: string) {
+    const element = document.createElement('html')
+    element.innerHTML = template;
+    return element;
+  }
+
+  private retreiveXTemplateFromHTML(templateElement : HTMLElement) : string {
+    this.debug('Retreiving x-template')
+    let elements = templateElement.querySelectorAll("[type='x-template']");
+
+    if(!elements || elements.length === 0) throw new Error('Could not find any x-template elements in the template');
+    if(!elements[0].innerHTML) throw new Error('x-template cannot be empty');
+    this.debug(`Found ${elements.length} elements`, elements)
+
+    return elements[0].innerHTML;
+  }
+
+  private registerXHeadScripts(templateElement : HTMLElement) : void {
+    // Remove any already loaded x-head scripts
+    document.head.querySelectorAll(`[id^="x-head-${this.state.id}"]`).forEach((i) => i.remove());
+
+    // Register elements
+    this.debug('Retreiving x-head')
+    const elements = templateElement.querySelectorAll("[type='x-head']");
+    this.debug(`Found ${elements.length} elements`, elements)
+    for (let i = 0; i < elements.length; i++) {
+      // Find all script elements 
+      const scripts = elements[i].getElementsByTagName('script');
+
+      for (let y = 0; y < scripts.length; y++) {
+        // Create a new script element, it will not run if just appending the parsed scripts
+        const script = document.createElement("script");
+        script.type = 'text/javascript'
+        script.async = true;
+        script.innerHTML = scripts[y].innerHTML;
+        script.id = `x-head-${this.state.id}`
+        // Append it to the head
+        document.head.appendChild(script);
+      }
+    }
+  }
+
+  private runXInjectCode(templateElement : HTMLElement) : void {
+    const Handlebars = handlebars;
     /*
       Run all injection scripts
     */
-    try {
-      this.debug('Retreiving x-inject')
-      const elements = element.querySelectorAll("[type='x-inject']");
-      this.debug(`Found ${elements.length} elements`, elements)
-      for (let i = 0; i < elements.length; i++) {
-        // Find all script elements 
-        const scripts = elements[i].getElementsByTagName('script');
-      
-        for (let y = 0; y < scripts.length; y++) {
-          if(!scripts[y]?.innerHTML) continue;
-          // Execute the code
-          eval(scripts[y].innerHTML)
-          
-        }
+    this.debug('Retreiving x-inject elements')
+    const elements = templateElement.querySelectorAll("[type='x-inject']");
+    this.debug(`Found ${elements.length} elements`, elements)
+    for (let i = 0; i < elements.length; i++) {
+      // Find all script elements 
+      const scripts = elements[i].getElementsByTagName('script');
+    
+      for (let y = 0; y < scripts.length; y++) {
+        if(!scripts[y]?.innerHTML) continue;
+        // Execute the code
+        eval(scripts[y].innerHTML)
       }
-    } catch (err) {
-      _errors.push(`Error parsing parsing x-inject:'\n${err.message}`)
+    }
+  }
+
+  private renderTemplate(templateString : string, data : object) {
+    const templateGenerator = handlebars.compile(templateString)
+    return templateGenerator({ dsData: data })
+  }
+
+  private async fetchData(options : ITemplateWebpartProps) {
+    const request : AxiosRequestConfig = {
+      url: options.dataUrl,
+      method: options.method,
+      data: options.body
     }
 
-    if(_errors.length > 0) return this.setState({errors: _errors});
-    /*
-      Output handlebars helpers
-    */
-   this.debug('Handlebars helpers', Handlebars.helpers);
+    this.debug('Fetching data from: ' + options.dataUrl)
 
-    /*
-      Render the template
-    */  
-    if(!this.state.html) {
-      // const html = Sjablong.replacePlaceholders(this.props.templateString, mockData)
-      const templateGenerator = Handlebars.compile(bodyTemplateString)
-      const html = templateGenerator({ dsData: mockData })
-      this.setState({
-        html
-      })
-    }
+    const { data } = await axios.request(request);
+    return data;
   }
 
   public render(): React.ReactElement<ITemplateWebpartProps> {
@@ -211,13 +311,13 @@ export default class TemplateWebpart extends React.Component<ITemplateWebpartPro
       debug
     } = this.props;
 
-    if(this.state.errors.length > 0) {
+    if(this.state.propErrors.length > 0 || this.state.error) {
       return (
         <div>
-          Fant { this.state.errors.length } feil
+          Fant { [...this.state.propErrors, this.state.error] } feil
           <ul>
             {
-              this.state.errors.map((i, index) => {
+              [...this.state.propErrors, this.state.error].map((i, index) => {
                 return(
                   <li key={index}>
                     { i || 'Ukjent feil' }
@@ -227,6 +327,14 @@ export default class TemplateWebpart extends React.Component<ITemplateWebpartPro
             }
           </ul>
         </div>
+      )
+    }
+
+    if(this.state.isLoading) {
+      return(
+        <>
+          <h1>Loading</h1>
+        </>
       )
     }
 
